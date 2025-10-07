973a77e34bbceca1393f47e84d7f525b55022941
abc-iimamritsar <75197107+abc-iimamritsar@users.noreply.github.com>
2025-10-08 00:08:56 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/**
+* Template Name: Maundy - v2.1.0
+* Template URL: https://bootstrapmade.com/maundy-free-coming-soon-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+  "use strict";
+
+  // Set the count down timer
+  if ($('.countdown').length) {
+    var count = $('.countdown').data('count');
+    var template = $('.countdown').html();
+    $('.countdown').countdown(count, function(event) {
+      $(this).html(event.strftime(template));
+    });
+  }
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  // document.getElementById("unlock").onclick = function() {
+    // if(document.getElementById("answer").value == ""){
+      // alert('Answer Field is empty');
+    // }
+    // else{
+      // document.location.href=document.getElementById("qno").value+'_'+document.getElementById("answer").value+'.html';
+    // }
+  // };
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+})(jQuery);
